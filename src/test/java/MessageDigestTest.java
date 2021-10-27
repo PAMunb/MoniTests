240f87e0253446669cce7f5b021620ff6afd0e07
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2021-10-27 18:29:25 -0300

    fixed exception handling strategy

--- src/test/java/MessageDigestTest.java
+++ src/test/java/MessageDigestTest.java
@@ -1,15 +1,26 @@
 import java.security.*;
-import java.io.*;
+
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class MessageDigestTest  {
-    @Test
+
+    @Test(expected = Test.None.class)
     public void validExecution() throws Exception {
         MessageDigest md = MessageDigest.getInstance("SHA-1");
         String Password = "secret";
         byte[] msg = Password.getBytes();
-	//        md.update(msg);
-        byte[] aMessageDigest = md.digest();        
+        md.update(msg);
+        byte[] aMessageDigest = md.digest();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void missingUpdateCall() throws Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        String Password = "secret";
+        byte[] msg = Password.getBytes();
+        byte[] aMessageDigest = md.digest();
     }
 
 }
