dcbb40b04a339a60103f89528f0230e0100c0349
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2021-11-03 18:11:03 -0300

    improve the MessageDigestSpec to support specific algorithms

--- src/test/java/MessageDigestTest.java
+++ src/test/java/MessageDigestTest.java
@@ -8,7 +8,7 @@ public class MessageDigestTest  {
 
     @Test(expected = Test.None.class)
     public void validExecution() throws Exception {
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
         String Password = "secret";
         byte[] msg = Password.getBytes();
         md.update(msg);
