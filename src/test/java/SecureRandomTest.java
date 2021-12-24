46d249928cc545e4d10fb3da8fd94edacb268a8a
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2021-12-24 16:10:25 -0300

    Integrated all test cases from the Crossing group.
    Three test cases still marked as ignored, since they look for
    invalid providers. This is easy to fix, we can for instance
    replace `(Provider)null` by `"SUN"`. Besides that, we have to
    include a new event for capturing this kind of `getInstance`
    event.

--- src/test/java/SecureRandomTest.java
+++ src/test/java/SecureRandomTest.java
@@ -113,4 +113,137 @@ public class SecureRandomTest {
 
     }
 
+    @Test
+    public void secureRandomValidTest6() throws NoSuchAlgorithmException {
+
+        int num = 0;
+
+        SecureRandom secureRandom1 = SecureRandom.getInstance("SHA1PRNG");
+        byte[] genSeed = secureRandom1.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom1);
+
+        SecureRandom secureRandom0 = SecureRandom.getInstance("SHA1PRNG");
+        secureRandom0.setSeed(genSeed);
+        Assertions.hasEnsuredPredicate(secureRandom0);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Ignore
+    public void secureRandomValidTest7() throws NoSuchAlgorithmException, NoSuchProviderException {
+
+        int num = 0;
+
+        SecureRandom secureRandom1 = SecureRandom.getInstance("SHA1PRNG");
+        byte[] genSeed = secureRandom1.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom1);
+
+        SecureRandom secureRandom0 = SecureRandom.getInstance("SHA1PRNG", (Provider) null);
+        secureRandom0.setSeed(genSeed);
+        Assertions.hasEnsuredPredicate(secureRandom0);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Test
+    public void secureRandomValidTest8() throws NoSuchAlgorithmException {
+
+        int num = 0;
+
+        SecureRandom secureRandom1 = SecureRandom.getInstance("SHA1PRNG");
+        byte[] genSeed = secureRandom1.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom1);
+
+        SecureRandom secureRandom0 = SecureRandom.getInstanceStrong();
+        secureRandom0.setSeed(genSeed);
+        Assertions.hasEnsuredPredicate(secureRandom0);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Test
+    public void secureRandomValidTest9() throws NoSuchAlgorithmException {
+
+        int num = 0;
+
+        SecureRandom secureRandom1 = SecureRandom.getInstance("SHA1PRNG");
+        byte[] genSeed = secureRandom1.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom1);
+
+        SecureRandom secureRandom0 = new SecureRandom();
+        secureRandom0.setSeed(genSeed);
+        Assertions.hasEnsuredPredicate(secureRandom0);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Test
+    public void secureRandomValidTest10() throws NoSuchAlgorithmException {
+
+        int num = 0;
+
+        SecureRandom secureRandom1 = SecureRandom.getInstance("SHA1PRNG");
+        byte[] genSeed = secureRandom1.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom1);
+
+        SecureRandom secureRandom0 = new SecureRandom(genSeed);
+        secureRandom0.setSeed(genSeed);
+        Assertions.hasEnsuredPredicate(secureRandom0);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Test
+    public void secureRandomValidTest11() throws NoSuchAlgorithmException {
+
+        long lSeed = 0;
+
+        SecureRandom secureRandom0 = SecureRandom.getInstance("SHA1PRNG");
+        secureRandom0.setSeed(lSeed);
+        Assertions.hasEnsuredPredicate(secureRandom0);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Test
+    public void secureRandomValidTest12() throws NoSuchAlgorithmException {
+
+        int num = 0;
+
+        SecureRandom secureRandom0 = SecureRandom.getInstance("SHA1PRNG");
+        byte[] genSeed = secureRandom0.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Ignore
+    public void secureRandomValidTest13() throws NoSuchAlgorithmException, NoSuchProviderException {
+
+        int num = 0;
+
+        SecureRandom secureRandom0 = SecureRandom.getInstance("SHA1PRNG", (Provider) null);
+        byte[] genSeed = secureRandom0.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
+    @Test
+    public void secureRandomValidTest14() throws NoSuchAlgorithmException {
+
+        int num = 0;
+
+        SecureRandom secureRandom0 = SecureRandom.getInstanceStrong();
+        byte[] genSeed = secureRandom0.generateSeed(num);
+        Assertions.hasEnsuredPredicate(genSeed);
+        Assertions.mustBeInAcceptingState(secureRandom0);
+
+    }
+
 }
