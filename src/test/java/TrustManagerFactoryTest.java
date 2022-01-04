358491fece2eebb2aaa07627a2977aa5ff110fe6
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-01-04 09:43:41 -0300

    fixed additional test cases

--- src/test/java/TrustManagerFactoryTest.java
+++ src/test/java/TrustManagerFactoryTest.java
@@ -39,8 +39,6 @@ public class TrustManagerFactoryTest  {
 	public void trustManagerFactoryValidTest1()
 			throws NoSuchAlgorithmException, IOException, KeyStoreException, CertificateException {
 
-
-
 		KeyStore keyStore0 = KeyStore.getInstance(keyStoreAlgorithm);
 		keyStore0.load(fileIn, passwordIn);
 		Assertions.hasEnsuredPredicate(keyStore0);
@@ -50,13 +48,11 @@ public class TrustManagerFactoryTest  {
 		trustManagerFactory0.init(keyStore0);
 		Assertions.hasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustBeInAcceptingState(trustManagerFactory0);
-
 	}
 
 	@Test
 	public void trustManagerFactoryValidTest2() throws NoSuchAlgorithmException, IOException, KeyStoreException,
 			CertificateException, NoSuchProviderException {
-
 		KeyStore keyStore0 = KeyStore.getInstance(keyStoreAlgorithm);
 		keyStore0.load(fileIn, passwordIn);
 		Assertions.hasEnsuredPredicate(keyStore0);
@@ -66,9 +62,10 @@ public class TrustManagerFactoryTest  {
 		trustManagerFactory0.init(keyStore0);
 		Assertions.hasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustBeInAcceptingState(trustManagerFactory0);
-
 	}
 
+	//TODO: we are ignoring this TC here because we do not have an specification
+	//   for PKIXBuilderParameters
 	@Ignore
 	public void trustManagerFactoryValidTest3() throws NoSuchAlgorithmException, IOException, KeyStoreException,
 			CertificateException, InvalidAlgorithmParameterException {
@@ -93,13 +90,11 @@ public class TrustManagerFactoryTest  {
 		trustManagerFactory0.init(certPathTrustManagerParameters0);
 		Assertions.hasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustBeInAcceptingState(trustManagerFactory0);
-
 	}
 
 	@Test
 	public void trustManagerFactoryValidTest4()
 			throws NoSuchAlgorithmException, IOException, KeyStoreException, CertificateException {
-
 		KeyStore keyStore0 = KeyStore.getInstance(keyStoreAlgorithm);
 		keyStore0.load(fileIn, passwordIn);
 		Assertions.hasEnsuredPredicate(keyStore0);
@@ -110,26 +105,25 @@ public class TrustManagerFactoryTest  {
 		TrustManager[] trustManager = trustManagerFactory0.getTrustManagers();
 		Assertions.hasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustBeInAcceptingState(trustManagerFactory0);
-
 	}
 
-	@Ignore
+	@Test
 	public void trustManagerFactoryValidTest5() throws NoSuchAlgorithmException, IOException, KeyStoreException,
 			CertificateException, NoSuchProviderException {
-
 		KeyStore keyStore0 = KeyStore.getInstance(keyStoreAlgorithm);
 		keyStore0.load(fileIn, passwordIn);
 		Assertions.hasEnsuredPredicate(keyStore0);
 		Assertions.mustBeInAcceptingState(keyStore0);
 
-		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX", (String) null);
+		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX", "SunJSSE");
 		trustManagerFactory0.init(keyStore0);
 		TrustManager[] trustManager = trustManagerFactory0.getTrustManagers();
 		Assertions.hasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustBeInAcceptingState(trustManagerFactory0);
-
 	}
 
+	//TODO: we are ignoring this TC here because we do not have an specification
+	//   for PKIXBuilderParameters
 	@Ignore
 	public void trustManagerFactoryValidTest6() throws NoSuchAlgorithmException, IOException, KeyStoreException,
 			CertificateException, InvalidAlgorithmParameterException {
@@ -155,40 +149,33 @@ public class TrustManagerFactoryTest  {
 		TrustManager[] trustManager = trustManagerFactory0.getTrustManagers();
 		Assertions.hasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustBeInAcceptingState(trustManagerFactory0);
-
 	}
 
 	@Test
 	public void trustManagerFactoryInvalidTest1() throws NoSuchAlgorithmException {
-
 		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX");
 		Assertions.notHasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustNotBeInAcceptingState(trustManagerFactory0);
-
 	}
 
-	@Ignore
+	@Test
 	public void trustManagerFactoryInvalidTest2() throws NoSuchAlgorithmException, NoSuchProviderException {
-
-		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX", (String) null);
+		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX", "SunJSSE");
 		Assertions.notHasEnsuredPredicate(trustManagerFactory0);
 		Assertions.mustNotBeInAcceptingState(trustManagerFactory0);
-
 	}
 
-	@Ignore
+	@Test(expected = java.lang.IllegalStateException.class)
 	public void trustManagerFactoryInvalidTest3() throws NoSuchAlgorithmException {
-
 		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX");
 		TrustManager[] trustManager = trustManagerFactory0.getTrustManagers();
 		Assertions.notHasEnsuredPredicate(trustManager);
 		Assertions.mustNotBeInAcceptingState(trustManagerFactory0);
-
 	}
 
-	@Ignore
+	@Test(expected = java.lang.IllegalStateException.class)
 	public void trustManagerFactoryInvalidTest4() throws NoSuchAlgorithmException, NoSuchProviderException {
-		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX", (String) null);
+		TrustManagerFactory trustManagerFactory0 = TrustManagerFactory.getInstance("PKIX", "SunJSSE");
 		TrustManager[] trustManager = trustManagerFactory0.getTrustManagers();
 		Assertions.notHasEnsuredPredicate(trustManager);
 		Assertions.mustNotBeInAcceptingState(trustManagerFactory0);
