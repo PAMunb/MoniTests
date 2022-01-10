837d3bff698be6d76ea760481819098a5fb8bad9
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2022-01-10 10:34:56 -0300

    fixing MessageDigest spec

--- src/test/java/MessageDigestTest.java
+++ src/test/java/MessageDigestTest.java
@@ -19,31 +19,14 @@ public class MessageDigestTest  {
     }
 
     @Test
-    public void validExecution() throws Exception {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        String Password = "secret";
-        byte[] msg = Password.getBytes();
-        md.update(msg);
-        byte[] out = md.digest();
-        Assertions.hasEnsuredPredicate(out);
-        Assertions.mustBeInAcceptingState(md);
-    }
-
-    @Test
-    public void missingUpdateCall() throws Exception {
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-        String Password = "secret";
-        byte[] msg = Password.getBytes();
-        byte[] aMessageDigest = md.digest();
-        Assertions.expectingNonEmptySetOfErrors(3);
-    }
-
-    @Test
     public void messageDigestValidTest1() throws NoSuchAlgorithmException {
+
         byte[] inbytearr = "secret".getBytes();
+
         MessageDigest messageDigest0 = MessageDigest.getInstance("SHA-256");
         byte[] out = messageDigest0.digest(inbytearr);
-        Assertions.notHasEnsuredPredicate(out);
-        Assertions.mustNotBeInAcceptingState(messageDigest0);
+        Assertions.hasEnsuredPredicate(out);
+        Assertions.mustBeInAcceptingState(messageDigest0);
+
     }
 }
